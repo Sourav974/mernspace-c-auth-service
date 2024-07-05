fb8a685ab0bccfe51b23d4cb7fafc3a5b7e6c85a
Sourav974 <souravyadav97@gmail.com>
2024-07-05 22:51:27 +0530

    test lint staged

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,9 @@ function login(username: string): string {
     // console.log(username);
     const user = {
         name: "Sourav",
-    }; const name = user["name"];
+    };
+    const name = user.name;
     return username + name;
 }
 
 login("Sourav");
-
-
-
