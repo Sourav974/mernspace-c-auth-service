4cf6cfaced47652776ecb8606b72d4dbb30b0788
Sourav974 <souravyadav97@gmail.com>
2024-07-25 06:44:13 +0530

    add email sanitization

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -213,4 +213,27 @@ describe("POST /auth/register", () => {
             expect(users).toHaveLength(0);
         });
     });
+
+    describe("Fields are not in proper format", () => {
+        it("should the email field", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Sourav ",
+                lastName: "Yadav",
+                email: " sourav@mern.space ",
+                password: "secret",
+            };
+
+            // Act
+            await request(app).post("/auth/register").send(userData);
+
+            // Assert
+
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            const user = users[0];
+
+            expect(user.email).toBe("sourav@mern.space");
+        });
+    });
 });
